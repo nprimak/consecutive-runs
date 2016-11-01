126903cef04716a26ac913f9052723c8f65ad46a
Nadya Primak <primak.nadya@gmail.com>
2016-11-01 17:05:17 -0400

    committing project

---
+++ README.md
@@ -0,0 +1,14 @@
+Write a javascript function named “find_consecutive_runs” that accepts as an argument a list of integers. 
+
+- It must find in that list all runs of 3 consecutive numbers that increase or decrease by 1 
+  - `[ x, x+1, x+2 ]` or `[ x, x-2, x-3 ]`
+- It should return the list indices of the first element of each run 
+- If there are no consecutive runs it should output `No Runs Found`
+
+Example Input: [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7, 8, 7]
+Results: [0, 4, 6, 7]
+
+## Tasks ##
+1. This is an example of terrible UI design.
+ - Make some improvements to the UI to make this more user friendly for input
+1. Implement the javascript function `searchConsecutiveNumbers` to achieve the solution for the question stated
---
+++ js.js
@@ -0,0 +1,46 @@
+var integerArray = [], indices = [];
+
+function addInteger(e) {
+    integerValue = document.getElementById('numberInput').value;
+    integerArray.push(integerValue);
+    document.getElementById('integerArray').value += (integerArray.length == 1 ? "" : ", " ) + integerValue;
+    document.getElementById("numberInput").value = "";
+
+};
+
+var reset = function () {
+    document.getElementById("indicesArray").innerHTML = "";
+    document.getElementById('integerArray').innerHTML = "";
+    integerArray = [];
+    indices = [];
+
+};
+
+function searchConsecutiveNumbers() {
+    indices = [];
+    convertArrayStringToIntegerArray();
+    for(var i = 0; i < integerArray.length; i++){
+        if(integerArray[i]+1 === integerArray[i+1]){
+            if(integerArray[i]+2 === integerArray[i+2]){
+                indices.push(i);
+                console.log("it is consecutive positive!");
+            }
+        }
+        if(integerArray[i]-1 === integerArray[i+1]){
+            if(integerArray[i]-2 === integerArray[i+2]){
+                indices.push(i);
+                console.log("it is consecutive negative!");
+            }
+        }
+    }
+    document.getElementById("indicesArray").getElementsByTagName("h1")[0].innerHTML = indices.join(", ");
+    if(indices.length === 0){
+        document.getElementById("indicesArray").getElementsByTagName("h1")[0].innerHTML = "NO INDICES FOUND";
+    }
+};
+
+function convertArrayStringToIntegerArray() {
+    for(var i = 0; i < integerArray.length; i++){
+        integerArray[i] = parseInt(integerArray[i], 10);
+    }
+}
